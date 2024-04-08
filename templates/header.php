894a63943d35f6d1dfd0604402f94ecc848988de
Mel <davidmel187@gmail.com>
2024-04-08 16:40:45 +0100

    Connected to database ,linked, added a read blog

--- templates/header.php
+++ templates/header.php
@@ -4,15 +4,28 @@
     <link rel="stylesheet" href="styles/header.css">
     <link rel="stylesheet" href="styles/body.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css" integrity="sha512-17AHGe9uFHHt+QaRYieK7bTdMMHBMi8PeWG99Mf/xEcfBLDCn0Gze8Xcx1KoSZxDnv+KnCC+os/vuQ7jrF/nkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        #h1 {
+            font-size: 25px;
+            line-height: 60px;
+            margin: 0;
+        }
+        nav a {
+            color: darkgoldenrod;
+        }
+
+
+
+    </style>
 </head>
 <body>
     <nav id="navigation">
         <div class="container">
             <div class="titleContainer">
-                <h1 class="titleh2">Mel-David Pizza</h1>
+                <h1 class="titleh2" id="h1"><a href="index.php">Mel-David Pizza</a></h1>
             </div>
             <div class="pizzaButton">
-                <a href="" class="titleA">ADD A PIZZA</a>
+                <a href="add.php" class="titleA">ADD A PIZZA</a>
             </div>
         </div>
     </nav>
